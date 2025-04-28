27f0eb4d28ad918b0211e9c0d8a61267271d486c
vaishnaviselvaraj05 <srivaishnavis009@gmail.com>
2025-04-28 06:53:36 +0530

    Time: 7 ms (31.41%) | Memory: 12 MB (29.96%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> map;
+        for(char c : magazine) map[c]++;
+        for(char c : ransomNote){
+            if(map[c] == 0) return false;
+            map[c] --;
+        }
+        return true;  
+    }
+};
