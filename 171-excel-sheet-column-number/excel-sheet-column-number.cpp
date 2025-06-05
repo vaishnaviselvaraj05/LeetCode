6e6f69c98ffd957f6b5fa9b6e7f0a79b35066daf
vaishnaviselvaraj05 <srivaishnavis009@gmail.com>
2025-06-05 11:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (18.26%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0;
+        for (auto c : columnTitle) {
+            res = res * 26 + (c - 'A' + 1);
+        }
+        return res;
+    }
+};
